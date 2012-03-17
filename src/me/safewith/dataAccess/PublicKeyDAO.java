76582663d0de95f2b79930eaf82e58d2c8643c9a
Tankred Hase <mail@tankredhase.de>
2012-03-18 01:10:46 +1100

    added GPL2 clauses to java code

--- src/me/safewith/dataAccess/PublicKeyDAO.java
+++ src/me/safewith/dataAccess/PublicKeyDAO.java
@@ -1,3 +1,23 @@
+/*
+ * SafeWith.me - store and share your files with OpenPGP encryption on any device via HTML5
+ *
+ * Copyright (c) 2012 Tankred Hase
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 package me.safewith.dataAccess;
 
 import java.util.List;
