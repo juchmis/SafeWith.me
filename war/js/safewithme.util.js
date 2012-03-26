632d0ec4222cb37771e8ab128c23ccfa6adce3b8
Tankred Hase <mail@tankredhase.de>
2012-03-27 08:57:26 +1100

    updated util unti test and optimized binStr2ArrBuf

--- war/js/safewithme.util.js
+++ war/js/safewithme.util.js
@@ -21,59 +21,17 @@
 function Util() {	
 	
 	/**
-	 * Converts a binary String from the FileReader Api to an ArrayBuffer
+	 * Converts a binary String from the FileReader Api to an ArrayBuffer [UInt8]
 	 */
 	this.binStr2ArrBuf = function(str) {
 		var b = new ArrayBuffer(str.length);
 		var buf = new Uint8Array(b);
 		
 		for(var i = 0; i < b.byteLength; i++){
-			buf[i] = str.charCodeAt(i)%256;
+			buf[i] = str.charCodeAt(i);
 		}
 
 		return b;
-	};
-
-	/**
-	 * Converts a UTF16 String to an ArrayBuffer
-	 */
-	this.str2ArrBuf = function(str) {
-		var b = new ArrayBuffer(str.length*2);
-		var dv = new DataView(b);
-		
-		for(var i = 0; i < b.byteLength; i+=2){
-		    var x = str.charCodeAt(i/2);
-		    var a = x%256;
-		    x -= a;
-		    x /= 256;
-			dv.setUint8(i, x);
-			dv.setUint8(i+1, a);
-		}
-
-		return b;
-	};
-
-	/**
-	 * Converts and ArrayBuffer to an UTF16 String
-	 */
-	this.arrBuf2Str = function(buf) {
-		var dv = new DataView(buf);
-		var s = "";
-		var high, low;
-		
-		for(var i = 0; i < buf.byteLength-2;){
-			high = dv.getUint8(i++);
-			low = dv.getUint8(i++);
-		    s += String.fromCharCode(high*256+low);
-		}
-		high = dv.getUint8(buf.byteLength-2);
-		low = 0;
-		if (buf.byteLength%2 === 0) {
-			low = dv.getUint8(buf.byteLength-1);
-		}
-	    s += String.fromCharCode(high*256+low);
-
-		return s;
 	};
 	
 	/**
--- war/test/util_test.js
+++ war/test/util_test.js
@@ -1,6 +1,8 @@
 module("Util");
 
-test("JQuery basic requirements",7 , function() {
+window.BlobBuilder =  window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder;
+
+test("JQuery and basic requirements", 8, function() {
 	ok( Array.prototype.push, "Array.push()" );
 	ok( Function.prototype.apply, "Function.apply()" );
 	ok( document.getElementById, "getElementById" );
@@ -8,21 +10,34 @@ test("JQuery basic requirements",7 , function() {
 	ok( RegExp, "RegExp" );
 	ok( jQuery, "jQuery" );
 	ok( $, "$" );
+	ok(window.BlobBuilder, "BlobBuilder");
 });
 
-test("String -> ArrayBuffer", 1, function() {
+asyncTest("String -> ArrayBuffer -> String", 2, function() {
 	var util = new Util();
+	
 	var input = "asdf";
-	var buf = util.str2ArrBuf(input);
-	var output = util.arrBuf2Str(buf);
-	equal(output, input);
+	var buf = util.binStr2ArrBuf(input);
+	ok(buf);
+	
+	var bb = new BlobBuilder();
+	bb.append(buf);
+	var blob = bb.getBlob('application/octet-stream');
+	
+	var reader = new FileReader();
+	reader.onload = function(event) {
+		var output = event.target.result;
+		equal(output, input);
+		
+		start();
+	};
+	reader.readAsBinaryString(blob);
 });
 
 asyncTest("Create URL", 2, function() {
 	var util = new Util();
 	
 	// Create a new Blob and write it to log.txt.
-	window.BlobBuilder =  window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder;
 	var bb = new BlobBuilder();
 	bb.append('asdf');
 	var blob = bb.getBlob('text/plain');
@@ -37,48 +52,3 @@ asyncTest("Create URL", 2, function() {
 		});
 	});
 });
-
-// asyncTest("ArrayBuffer -> String", 2, function() {
-// 	var util = new Util();
-// 	PDFJS.getPdf('test/helloworld.pdf', function(data) {
-// 		ok(data);
-// 	 	var str = util.arrBuf2Str(data);
-// 		var buf = util.str2ArrBuf(str);
-// 	 	var str2 = util.arrBuf2Str(buf);
-// 		equal(str, str2);
-// 
-// 		start();
-// 	});
-// });
-// 
-// asyncTest("ArrayBuffer -> Unicode -> Base64 -> Encrypt -> Decrypt -> Base64 -> Unicode -> ArrayBuffer", 11, function() {
-// 	var crypto = new Crypto();
-// 	crypto.init("test@asdf.com");
-// 	var util = new Util();
-// 	
-// 	PDFJS.getPdf('test/helloworld.pdf', function(data) {
-// 		ok(data, "ArrayBuffer size: " + data.byteLength);
-// 		
-// 		// encode and encrypt
-// 		var unicode = util.arrBuf2Str(data);
-// 		ok(unicode, "Unicode size: "+ unicode.length*2);
-// 		var base64 = util.Base64.encode(unicode); // btoa(unicode);
-// 		ok(base64, "Base64 size: "+ base64.length*2);
-// 		var cipher = crypto.encrypt(base64, crypto.getPublicKey());
-// 		ok(cipher, "Cipher size: " + cipher.length*2);
-// 		
-// 		// decrypt and decode
-// 		var base64Dec = crypto.decrypt(cipher, crypto.getPrivateKey(), '');
-// 		ok(base64Dec, "Base64Dec size: " + base64Dec.length*2);
-// 		equal(base64.length, base64Dec.length);
-// 		equal(base64, base64Dec);
-// 		var unicodeDec = util.Base64.decode(base64Dec); // atob(base64);
-// 		ok(unicodeDec, "UnicodeDec size: " + unicodeDec.length*2);
-// 		equal(unicode.length, unicodeDec.length);
-// 		equal(unicode, unicodeDec);
-// 		var bufDec = util.str2ArrBuf(unicodeDec);
-// 		ok(bufDec, "Decrypted ArrayBuffer size: " + bufDec.byteLength);
-// 
-// 		start();
-// 	});
-// });
