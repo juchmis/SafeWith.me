79ed00587f2de05c1a623db462652bacd035d218
Tankred Hase <mail@tankredhase.de>
2012-04-06 11:58:25 +0800

    update to App Engine SDK 1.6.4

--- res/run_server.sh
+++ res/run_server.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 cd ..
 
 # Set to current google appenginge SDK directory
-GAE_SDK=/Applications/eclipse/plugins/com.google.appengine.eclipse.sdkbundle_1.6.1.v201201120043r37/appengine-java-sdk-1.6.1
+GAE_SDK=/Applications/eclipse/plugins/com.google.appengine.eclipse.sdkbundle_1.6.4.v201203300216r37/appengine-java-sdk-1.6.4
 
 # Start with '--wipe' to delete datastore and blobstore to start with a fresh server
 if [ "$1" = "--wipe" ]; then
