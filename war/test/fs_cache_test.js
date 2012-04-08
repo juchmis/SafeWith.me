775691d7a033f669d7a1a2588aa031815ca8e8d5
Tankred Hase <mail@tankredhase.de>
2012-04-09 01:10:16 +0800

    added unit test for bucketFS cache

--- war/test/fs_cache_test.js
+++ war/test/fs_cache_test.js
@@ -1,8 +1,21 @@
 module("FS Cache");
 
-test("Bucket FS Cache", 1, function() {
+test("Bucket FS Cache", 6, function() {
 	ok(!BUCKETCACHE.current());
 	ok(!BUCKETCACHE.current());
 	
-	BUCKETCACHE.putFS()
+	var bucket = {
+		encryptedFS : 'asdfasdf'
+	};
+	
+	var bucketFS = new FS.BucketFS('testId', 'Test BucketFS', 'test@asdf.com');
+	
+	BUCKETCACHE.putFS(bucket, bucketFS);
+	
+	var currentBucket = BUCKETCACHE.current().bucket;
+	var currentBucketFS = BUCKETCACHE.current().bucketFS;
+	ok(currentBucket);
+	ok(currentBucketFS);
+	equal(currentBucket.encryptedFS, 'asdfasdf');
+	equal(currentBucketFS.name, 'Test BucketFS');
 });
