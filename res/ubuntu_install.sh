1d6ad4e76e5d7eaf63694de7afcbfe51d8b98835
Tankred Hase <mail@tankredhase.de>
2012-05-03 18:45:51 +0800

    upsart script update

--- res/ubuntu_install.sh
+++ res/ubuntu_install.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 
-cd `dirname $0`
+# set directories
+INSTALL_DIR=/var/www/safewithme
+LOG_FILE=/var/log/safewithme.sys.log
 
 # install required packages
 sudo apt-get update
 sudo apt-get install nodejs npm
 
+cd `dirname $0`
+
 # install node depencies
 cd ../src
 npm install
-
 
 # install upstart script for server daemon
 cd ../res
 sudo cp safewithme.conf /etc/init/
 
 # install webservice to /var/www/
-echo installing to /var/www/safewithme
+echo installing to $INSTALL_DIR
 cd ..
-sudo mkdir -p /var/www/safewithme
-sudo cp -r src /var/www/safewithme
-sudo mkdir -p /var/www/safewithme/res
-sudo cp res/run_server.sh /var/www/safewithme/res
+sudo mkdir -p $INSTALL_DIR
+sudo cp -r src $INSTALL_DIR
+sudo mkdir -p $INSTALL_DIR/res
+sudo cp res/run_server.sh $INSTALL_DIR/res
 
 # copy ssl files (only required the first time)
-sudo cp -r ssl /var/www/safewithme
+sudo cp -r ssl $INSTALL_DIR
 
 #create log file
-echo creating log-file /var/log/safewithme.sys.log
-sudo touch /var/log/safewithme.sys.log
+echo creating log-file $LOG_FILE
+sudo touch $LOG_FILE
 
 # channge permission
-sudo chown www-data:www-data /var/log/safewithme.sys.log
-sudo chown -R www-data:www-data /var/www/safewithme
-sudo chmod -R 500 /var/www/safewithme/ssl
-
-
-
+sudo chown www-data:www-data $LOG_FILE
+sudo chown -R www-data:www-data $INSTALL_DIR
+sudo chmod -R 500 $INSTALL_DIR/ssl
