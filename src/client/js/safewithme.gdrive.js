3d61cbd2aa993c0d9b10ab3f795391c55c0b4c5b
Tankred Hase <mail@tankredhase.de>
2012-05-13 12:51:23 +0800

    added crud methods for google drive

--- src/client/js/safewithme.gdrive.js
+++ src/client/js/safewithme.gdrive.js
@@ -21,7 +21,7 @@
 /**
  * A wrapper for Google Drive communication
  */
-var GoogleDrive = function(oauth, server) {
+var GoogleDrive = function(oauth, util, server) {
 	var self = this;
 	
 	var driveBaseUri = 'https://www.googleapis.com/drive/v1/files';
@@ -32,7 +32,12 @@ var GoogleDrive = function(oauth, server) {
 			// parse the query string
 			var oauthParams = oauth.oauth2Callback();
 			// $('#loginStatus').html(JSON.stringify(oauthParams));
-			self.insert(null, oauthParams);
+			
+			// test upload new blob
+			var contents = 'Hello, World!';
+			var buf = util.binStr2ArrBuf(contents);
+			var blob = util.arrBuf2Blob(buf, 'text/plain');
+			self.uploadBlob(blob, oauthParams, md5(contents));
 			
 		} else {
 			// set the login link
@@ -41,45 +46,84 @@ var GoogleDrive = function(oauth, server) {
 		}
 	};
 	
-	self.insert = function(blob, oauthParams, md5, callback, errCallback) {
-		// first get upload url from blobstore
+	/**
+	 * Upload a new file blob to Google Drive by first allocating a
+	 * new file resource (POST) and then uploading the file contents (PUT)
+	 */
+	self.uploadBlob = function(blob, oauthParams, md5, callback, errCallback) {
+		// first POST new drive file to allocate resource
 		server.xhr({
 			type: 'POST',
 			uri: driveBaseUri,
 			contentType: 'application/json',
 			auth: oauthParams.token_type + ' ' + oauthParams.access_token,
 			body: JSON.stringify({
-				title: 'safe_file',
-				mimeType: 'application/octet-stream'
+				title: 'encrypted_blob_' + md5,
+				mimeType: blob.type
 			}),
 			expected: 200,
-			success: function(resp) {
-				postBlob();
+			success: function(created) {
+				uploadBlob(created.id);
 			},
 			error: function(e) {
 				errCallback(e);
 			}
 		});
 
-		// upload blob to blobstore
-		function postBlob(postUrl) {
+		// then PUT the file contents
+		function uploadBlob(fileId) {
 			var formData = new FormData();	// multipart/form-data
 			formData.append('file', blob);
-			formData.append('md5', md5);
 
-			// server.xhr({
-			// 	type: 'POST',
-			// 	uri: postUrl,
-			// 	body: formData,
-			// 	expected: 201,
-			// 	success: function(resp) {
-			// 		callback(resp.blobKey);
-			// 	},
-			// 	error: function(e) {
-			// 		errCallback(e);
-			// 	}
-			// });
+			server.xhr({
+				type: 'PUT',
+				uri: driveBaseUri + '/' + fileId,
+				body: formData,
+				expected: 200,
+				success: function(resp) {
+					callback(fileId);
+				},
+				error: function(e) {
+					errCallback(e);
+				}
+			});
 		}
+	};
+	
+	/**
+	 * Download a blob from Google Drive
+	 */
+	self.downloadBlob = function(fileId, callback, errCallback) {
+		server.xhr({
+			type: 'GET',
+			uri: driveBaseUri + '/' + fileId,
+			responseType: 'arraybuffer',
+			expected: 200,
+			success: function(resp) {
+				var blob = util.arrBuf2Blob(resp, 'application/octet-stream');
+				callback(blob);
+			},
+			error: function(e) {
+				errCallback(e);
+			}
+		});
+	};
+	
+	/**
+	 * Deletes a blob from Google Drive
+	 */
+	self.deleteBlob = function(fileId, callback, errCallback) {
+		server.xhr({
+			type: 'DELETE',
+			uri: driveBaseUri + '/' + fileId,
+			expected: 200,
+			success: function(resp) {
+				callback(resp);
+			},
+			error: function(e) {
+				errCallback(e);
+			}
+		});
 	};
 	
 };
