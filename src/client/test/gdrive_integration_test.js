e5d3332a623e65e4ca5c2f338fa00befdaed60ed
Tankred Hase <mail@tankredhase.de>
2012-05-14 10:04:24 +0800

    gdrive integration tests

---
+++ src/client/test/gdrive_integration_test.js
@@ -0,0 +1,35 @@
+module("Integration - Google Drive");
+
+asyncTest("Upload, Download, Delete blob", 4, function() {
+	var util = new Util(window, uuid);
+	var server = new Server(util);
+	var oauth = new OAuth(window);
+	var gdrive = new GoogleDrive(util, server);
+	
+	var oauthParams = oauth.oauth2Callback();
+	if (oauthParams) {
+		// if oauth params are present, do test
+		testUpload();
+		
+	} else {
+		// test failed
+		start();
+		return;
+	}
+	
+	function testUpload() {
+		// create test blob
+		var contents = 'Hello, World!';
+		var buf = util.binStr2ArrBuf(contents);
+		var blob = util.arrBuf2Blob(buf, 'text/plain');
+		
+		// upload to google drive
+		gdrive.uploadBlob(blob, oauthParams, md5(contents), function(fileId) {
+			
+		}, function(err) {
+			// test failed
+			start();
+			return;
+		});
+	}
+});
